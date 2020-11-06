e0f919df3d6f02431d7b5dae12de4140b2625f10
d3vkk <donaldkk22@yahoo.com>
2020-11-06 13:22:19 +0300

    chore: Add tailwindcss dependency

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: [
+      './src/**/*.vue',
+      './src/main.js',
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
