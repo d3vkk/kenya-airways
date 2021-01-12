39d8761ec80a48290b87a5240812f1c8d32d7dc5
d3vkk <donaldkk22@yahoo.com>
2021-01-12 13:56:45 +0300

    refactor: Change landing page layout

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,36 +1,25 @@
 <template>
   <div>
     <NavBar />
-    <div class="mt-1">
-      <img
-        class="relative"
-        src="img/hero.webp"
-        alt="Kenya Airways Hero"
-        style="filter: grayscale(50%)"
-      />
-      <div>
-        <div id="hero-title" class="absolute">Kenya Airways</div>
-        <div id="hero-slogan" class="absolute">The Pride of Africa</div>
+    <div id="hero-image" class="flex flex-col items-center my-1">
+      <div class="p-4">
+        <div id="hero-title">Kenya Airways</div>
+        <div id="hero-slogan">The Pride of Africa</div>
       </div>
-    </div>
     <div class="flex flex-row items-center justify-center my-8">
-      <form
-        @submit.prevent="searchFlight()"
-        class="absolute p-4 bg-white shadow-lg"
-        style="top: 26rem"
-      >
+      <form @submit.prevent="searchFlight()" class="p-2 bg-white rounded-lg shadow-lg">
         <div class="flex flex-row">
           <div class="w-1/3 p-2 mt-4 mb-4">
             <div class="mb-2">From</div>
-            <input type="text" autocomplete required v-model="fromLocation" />
+            <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none" type="text" autocomplete required v-model="fromLocation" />
           </div>
           <div class="w-1/3 p-2 mt-4 mb-4">
             <div class="mb-2">To</div>
-            <input type="text" autocomplete required v-model="toLocation" />
+            <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none" type="text" autocomplete required v-model="toLocation" />
           </div>
           <div class="w-1/3 p-2 mt-4 mb-4">
             <div class="mb-2">Depart</div>
-            <input type="time" autocomplete required v-model="departTime" />
+            <input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none" type="time" autocomplete required v-model="departTime" />
           </div>
         </div>
         <div class="flex flex-row">
@@ -69,6 +58,7 @@
         </div>
       </form>
     </div>
+    </div>
     <FooterSection />
   </div>
 </template>
@@ -95,12 +85,4 @@ export default {
 </script>
 
 <style scoped>
-input[type="time"],
-input[type="text"],
-select {
-  @apply shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight;
-}
-select {
-  @apply bg-white text-black;
-}
 </style>
