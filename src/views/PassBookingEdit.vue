94f6d5cd59e280d199944cfe520f95ca83b3024f
d3vkk <donaldkk22@yahoo.com>
2021-01-27 14:10:47 +0300

    refactor: Add navbar to passenger booking edit

--- src/views/PassBookingEdit.vue
+++ src/views/PassBookingEdit.vue
@@ -1,65 +1,71 @@
 <template>
-  <div class="container max-w-xs p-8">
-    <div class="pl-2 text-2xl">Edit Booking</div>
-    <form @submit.prevent="editBooking">
-      <div class="p-2 mt-4 mb-4">
-        <div class="mb-2">Name</div>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-          type="text"
-          autocomplete
-          required
-          v-model="nameInput"
-        />
-      </div>
-      <div class="p-2 mt-4 mb-4">
-        <div class="mb-2">Date of Birth</div>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-          type="date"
-          autocomplete
-          required
-          v-model="dob"
-        />
-      </div>
-      <div class="p-2 mt-4 mb-4">
-        <div class="mb-2">Phone Number</div>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-          type="text"
-          autocomplete
-          required
-          v-model="phoneNumber"
-        />
-      </div>
-      <div class="p-2 mt-4 mb-4">
-        <div class="mb-2">Email</div>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-          type="email"
-          autocomplete
-          required
-          v-model="email"
-        />
-      </div>
-      <div class="flex flex-row">
-        <button
-          class="p-2 ml-3 text-sm font-medium tracking-wide text-white bg-black rounded-md hover:bg-red-800"
-          type="submit"
-        >
-          Edit
-        </button>
-        <router-link class="p-2" to="/dashboard">Cancel</router-link>
-      </div>
-    </form>
-  </div>
+ <div>
+    <NavBar />
+    <div class="container max-w-xs p-8">
+      <div class="pl-2 text-2xl">Edit Booking</div>
+      <form @submit.prevent="editBooking">
+        <div class="p-2 mt-4 mb-4">
+          <div class="mb-2">Name</div>
+          <input
+            class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none"
+            type="text"
+            autocomplete
+            required
+            v-model="nameInput"
+          />
+        </div>
+        <div class="p-2 mt-4 mb-4">
+          <div class="mb-2">Date of Birth</div>
+          <input
+            class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none"
+            type="date"
+            autocomplete
+            required
+            v-model="dob"
+          />
+        </div>
+        <div class="p-2 mt-4 mb-4">
+          <div class="mb-2">Phone Number</div>
+          <input
+            class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none"
+            type="text"
+            autocomplete
+            required
+            v-model="phoneNumber"
+          />
+        </div>
+        <div class="p-2 mt-4 mb-4">
+          <div class="mb-2">Email</div>
+          <input
+            class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none"
+            type="email"
+            autocomplete
+            required
+            v-model="email"
+          />
+        </div>
+        <div class="flex flex-row">
+          <button
+            class="p-2 ml-3 text-sm font-medium tracking-wide text-white bg-black rounded-md hover:bg-red-800"
+            type="submit"
+          >
+            Edit
+          </button>
+          <router-link class="p-2" to="/dashboard">Cancel</router-link>
+        </div>
+      </form>
+ </div>
 </template>
 
 <script>
 import "../../public/vendor/tailwind.min.css";
+import NavBar from "../layouts/NavBar";
 
 export default {
   name: "pass-booking-edit",
+  components: {
+    NavBar,
+  },
   data() {
     return {
       editPassengerData: {},
