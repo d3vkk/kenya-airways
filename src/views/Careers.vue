f9f7004e57979f3419e3ac2facc48385a7c95040
d3vkk <donaldkk22@yahoo.com>
2021-01-27 11:06:09 +0300

    refactor: Use tailwind css, remove cards

--- src/views/Careers.vue
+++ src/views/Careers.vue
@@ -1,159 +1,145 @@
 <template>
   <div>
-    <header id="header" class="header">
-      <div class="header-content">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="text-container">
-                <h2>Explore a world of opportunities</h2>
-                <p class="p-heading p-large" style="margin-bottom: 0%">
-                  The KQ Group offers a wide range of careers, whether you’re
-                  looking to join our pilots, engineering or cabin crew and air
-                  services teams. Browse our current vacancies and find out what
-                  it’s like to work for a dynamic and multicultural business.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-    <div class="container mt-4">
-      <div class="card-deck">
-        <div class="card card1">
-          <div class="mb-4">
-            <a href="job.html" style="text-decoration: none"
-              ><div
-                class="mx-auto text-white card col-sm-4"
-                style="
-                  background-color: #e91d24;
-                  height: 30px;
-                  margin-top: 10rem;
-                "
-              >
-                <p class="mx-auto card-text">Vacancies</p>
-              </div></a
-            >
-          </div>
-        </div>
-        <div class="card card2">
-          <div class="mb-4">
-            <a href="job.html" style="text-decoration: none"
-              ><div
-                class="mx-auto text-white card col-sm-4"
-                style="
-                  background-color: #e91d24;
-                  height: 30px;
-                  margin-top: 10rem;
-                "
-              >
-                <p class="mx-auto card-text">Join Us</p>
-              </div></a
-            >
-          </div>
-        </div>
-        <div class="card card3">
-          <div class="mb-4">
-            <a href="job.html" style="text-decoration: none"
-              ><div
-                class="mt-5 text-white justify-content-center btn col-sm-4 d-flex align-items-center"
-                style="background-color: #e91d24"
-              >
-                <p class="mx-auto card-text">Internal&nbsp;Openings</p>
-              </div></a
-            >
-          </div>
-        </div>
-      </div>
-      <div class="mt-4 card" style="border: none">
-        <div class="mx-5">
-          <h3>Jobs</h3>
-          <a href="job.html" class="joblist"
-            ><p style="margin-bottom: 0%">
-              Pacific Coordination Center Senior Manager
+    <NavBar />
+    <div>
+      <header id="header" class="header my-1">
+        <div class="header-content">
+          <div class="text-white">
+            <h2 style="font-size: 2.5rem">Explore a world of opportunities</h2>
+            <p>
+              The KQ Group offers a wide range of careers, whether you’re
+              looking to join our pilots, engineering or cabin crew and air
+              services teams. Browse our current vacancies and find out what
+              it’s like to work for a dynamic and multicultural business.
             </p>
-            <p>NARITA, Japan</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">
-              Talent Acquisition Specialist - Applicant Tracking System
-              Configuration
-            </p>
-            <p>ATLANTA, Georgia</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Lead Cargo Maintenance Technician</p>
-            <p>LOS ANGELES, California</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Cargo Maintenance Technician</p>
-            <p>DETROIT, Michigan</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Lead Cargo Maintenance Technician</p>
-            <p>SEATTLE, Washington</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Cargo Maintenance Technician</p>
-            <p>LOS ANGELES, California</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Lead Cargo Maintenance Technician</p>
-            <p>MINNEAPOLIS, Minnesota</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Cargo Maintenance Technician</p>
-            <p>SEATTLE, Washington</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Cargo Maintenance Technician</p>
-            <p>ATLANTA, Georgia</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Lead Cargo Maintenance Technician</p>
-            <p>ATLANTA, Georgia</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Lead Cargo Maintenance Technician</p>
-            <p>SALT LAKE CITY, Utah</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Cargo Maintenance Technician</p>
-            <p>SALT LAKE CITY, Utah</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Cargo Maintenance Technician</p>
-            <p>MINNEAPOLIS, Minnesota</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Lead Cargo Maintenance Technician</p>
-            <p>DETROIT, Michigan</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Senior Business Analyst, API</p>
-            <p>ATLANTA, Georgia</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">Senior Analyst- Government Affairs</p>
-            <p>WASHINGTON, District Of Columbia</p>
-          </a>
-          <a href="job.html" class="joblist">
-            <p style="margin-bottom: 0%">
-              Stationary Ground Maintenance Technician, LGA
-            </p>
-            <p>NEW YORK, New York</p>
-          </a>
+          </div>
+        </div>
+      </header>
+      <div>
+        <div class="px-3 py-6">
+          <h1 style="font-size: 2.5rem">Jobs</h1>
+          <div class="joblist py-3">
+            <a href="job.html"
+              ><p>Pacific Coordination Center Senior Manager</p>
+              <p>NARITA, Japan</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>
+                Talent Acquisition Specialist - Applicant Tracking System
+                Configuration
+              </p>
+              <p>ATLANTA, Georgia</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Lead Cargo Maintenance Technician</p>
+              <p>LOS ANGELES, California</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Cargo Maintenance Technician</p>
+              <p>DETROIT, Michigan</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Lead Cargo Maintenance Technician</p>
+              <p>SEATTLE, Washington</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Cargo Maintenance Technician</p>
+              <p>LOS ANGELES, California</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Lead Cargo Maintenance Technician</p>
+              <p>MINNEAPOLIS, Minnesota</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Cargo Maintenance Technician</p>
+              <p>SEATTLE, Washington</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Cargo Maintenance Technician</p>
+              <p>ATLANTA, Georgia</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Lead Cargo Maintenance Technician</p>
+              <p>ATLANTA, Georgia</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Lead Cargo Maintenance Technician</p>
+              <p>SALT LAKE CITY, Utah</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Cargo Maintenance Technician</p>
+              <p>SALT LAKE CITY, Utah</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Cargo Maintenance Technician</p>
+              <p>MINNEAPOLIS, Minnesota</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Lead Cargo Maintenance Technician</p>
+              <p>DETROIT, Michigan</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Senior Business Analyst, API</p>
+              <p>ATLANTA, Georgia</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Senior Analyst- Government Affairs</p>
+              <p>WASHINGTON, District Of Columbia</p>
+            </a>
+          </div>
+          <div class="joblist py-3">
+            <a href="job.html">
+              <p>Stationary Ground Maintenance Technician, LGA</p>
+              <p>NEW YORK, New York</p>
+            </a>
+          </div>
         </div>
       </div>
     </div>
+    <FooterSection />
   </div>
 </template>
 
 <script>
-import "../../public/vendor/bootstrap.min.css";
+import NavBar from "../layouts/NavBar";
+import FooterSection from "../layouts/FooterSection";
 
-export default {};
+export default {
+  components: {
+    NavBar,
+    FooterSection,
+  },
+};
 </script>
 
 <style scoped>
@@ -166,7 +152,7 @@ export default {};
 
 .header .header-content {
   padding-top: 20rem;
-  padding-bottom: 2.125rem;
+  padding-bottom: 2.5rem;
   text-align: center;
 }
 
@@ -185,28 +171,7 @@ export default {};
   margin-bottom: 1.25rem;
 }
 
-.card1 {
-  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)),
-    url("../../public/img/careers-join-us.jpg") center center no-repeat;
-  background-size: cover;
-  height: 200px;
-}
-
-.card2 {
-  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)),
-    url("../../public/img/careers-pilot.jpg") center center no-repeat;
-  background-size: cover;
-  height: 200px;
-}
-
-.card3 {
-  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)),
-    url("../../public/img/careers-cabin-crew.jpg") center center no-repeat;
-  background-size: cover;
-  height: 200px;
-}
-
-.joblist {
-  color: black;
+.joblist a:hover {
+  color: #e91d24;
 }
 </style>
