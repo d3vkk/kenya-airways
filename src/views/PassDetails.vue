7f28ffeeb7337439b98d8ce53ec6b7e3724ff501
d3vkk <donaldkk22@yahoo.com>
2021-01-28 10:50:55 +0300

    refactor: Input multiple passengers, bags & pets
    For passenger details

--- src/views/PassDetails.vue
+++ src/views/PassDetails.vue
@@ -6,42 +6,73 @@
       <div class="container py-5">
         <div class="card card1 col-sm-8">
           <div class="mt-4">
-            <h3 style="float: left">Passenger One</h3>
+            <h3 style="float: left">Passenger {{ passengerNumber }}</h3>
           </div>
-          <form class="mx-3">
+          <form class="mx-3" @submit.prevent="nextPassenger">
             <div class="form-group w-100">
               <label>Name</label>
-              <input type="text" class="form-control" required />
+              <input
+                type="text"
+                class="form-control"
+                required
+                v-model="inputName"
+              />
             </div>
             <div class="form-group w-100">
               <label>Email</label>
-              <input type="email" class="form-control" required />
+              <input
+                type="email"
+                class="form-control"
+                required
+                v-model="email"
+              />
             </div>
             <div class="form-group w-100">
               <label>Phone Number</label>
-              <input type="text" class="form-control" required />
+              <input
+                type="text"
+                class="form-control"
+                required
+                v-model="phoneNumber"
+              />
             </div>
             <div class="form-group w-100">
               <label>ID/Passport No.</label>
-              <input type="text" class="form-control" required />
+              <input
+                type="text"
+                class="form-control"
+                required
+                v-model="passport"
+              />
             </div>
             <div class="form-group w-100">
               <label>Date Of Birth</label>
-              <input type="date" class="form-control" required />
+              <input type="date" class="form-control" required v-model="dob" />
             </div>
             <div class="form-group w-100">
               <label>City</label>
-              <input type="text" class="form-control" required />
+              <input type="text" class="form-control" required v-model="city" />
             </div>
             <div class="form-group w-100">
               <label>Country</label>
-              <input type="text" class="form-control" id="inputState" />
+              <input
+                type="text"
+                class="form-control"
+                required
+                v-model="country"
+              />
             </div>
             <div class="mb-3">
-              <button type="button" class="mr-3 btn btn-dark">
+              <button type="submit" class="mr-3 btn btn-dark">
                 Next Passenger
               </button>
-              <button type="button" class="btn btn-success">Done</button>
+              <button
+                type="button"
+                class="btn btn-success"
+                @click="doneWithPassengers()"
+              >
+                Done
+              </button>
             </div>
           </form>
           <h2 class="mt-4 mb-3">Baggage and Pets</h2>
@@ -49,19 +80,21 @@
             <div class="mb-3">
               <div>Number of Bags between 23 & 35kgs</div>
               <div style="color: grey">2,500 per bag</div>
-              <input
-                type="number"
-                class="form-control w-100"
-                id="inputbaggage"
-              />
+              <input type="number" class="form-control w-100" v-model="bags" />
             </div>
             <div class="mb-3">
               <div>Number of Pets:</div>
               <div style="color: grey">1000 per pet</div>
-              <input type="number" class="form-control w-100" id="inputPet" />
+              <input type="number" class="form-control w-100" v-model="pets" />
             </div>
             <div class="mb-3">
-              <button type="button" class="btn btn-success">Done</button>
+              <button
+                type="button"
+                class="btn btn-success"
+                @click="doneWithBaggageAndPets()"
+              >
+                Done
+              </button>
             </div>
           </form>
         </div>
@@ -88,6 +121,18 @@ export default {
       stepTitle: "Passenger Details, Baggage and Pets",
       stepNumber: 3,
       stepInfo: null,
+      ticketData: null,
+      inputName: null,
+      email: null,
+      phoneNumber: null,
+      passport: null,
+      dob: null,
+      city: null,
+      country: null,
+      savePassengerData: [],
+      passengerNumber: 1,
+      bags: null,
+      pets: null,
     };
   },
   mounted() {
@@ -95,6 +140,44 @@ export default {
       stepTitle: this.stepTitle,
       stepNumber: this.stepNumber,
     };
+    const ticketDataStored = JSON.parse(localStorage.getItem("ticketData"));
+    this.ticketData =
+      ticketDataStored == null ? [] : ticketDataStored.ticketData;
+  },
+  methods: {
+    nextPassenger() {
+      this.savePassengerData[this.savePassengerData.length] = {
+        name: this.inputName,
+        dob: this.dob,
+        phoneNumber: this.phoneNumber,
+        email: this.email,
+      };
+      this.clearForm();
+      this.passengerNumber++;
+    },
+    clearForm() {
+      this.inputName = null;
+      this.email = null;
+      this.phoneNumber = null;
+      this.passport = null;
+      this.dob = null;
+      this.city = null;
+      this.country = null;
+    },
+    doneWithPassengers() {
+      this.ticketData.passengerData = this.savePassengerData;
+      localStorage.setItem(
+        "ticketData",
+        JSON.stringify({ ticketData: this.ticketData })
+      );
+    },
+    doneWithBaggageAndPets() {
+      this.ticketData.petCount = this.pets;
+      localStorage.setItem(
+        "ticketData",
+        JSON.stringify({ ticketData: this.ticketData })
+      );
+    },
   },
 };
 </script>
