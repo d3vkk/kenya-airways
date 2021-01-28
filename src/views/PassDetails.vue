da9f03cf54b7948a735b928de038e4582737f4e9
d3vkk <donaldkk22@yahoo.com>
2021-01-28 10:05:00 +0300

    refactor: Style form, change fields

--- src/views/PassDetails.vue
+++ src/views/PassDetails.vue
@@ -9,124 +9,55 @@
             <h3 style="float: left">Passenger One</h3>
           </div>
           <form class="mx-3">
-            <label>Name</label>
-            <div class="mb-3 form-row">
-              <div class="col">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="First name"
-                />
-              </div>
-              <div class="col">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Second name"
-                />
-              </div>
-              <div class="col">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Last name"
-                />
-              </div>
+            <div class="form-group w-100">
+              <label for="inputName">Name</label>
+              <input type="text" class="form-control" required />
             </div>
-            <div class="mb-3 form-row">
-              <div class="form-group col-md-6">
-                <label for="inputGender">Gender</label>
-                <select id="inputGender" class="form-control">
-                  <option selected>Choose...</option>
-                  <option>Male</option>
-                  <option>Female</option>
-                </select>
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputID">ID/passport No.</label>
-                <input type="text" class="form-control" id="inputID" />
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputPhone">Phone No</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="inputPhone"
-                  placeholder="Phone"
-                />
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">Email</label>
-                <input
-                  type="email"
-                  class="form-control"
-                  id="inputEmail4"
-                  placeholder="Email"
-                />
-              </div>
+            <div class="form-group w-100">
+              <label for="inputEmail4">Email</label>
+              <input type="email" class="form-control" required />
             </div>
-
-            <div class="mb-3 form-row">
-              <div class="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" class="form-control" id="inputCity" />
-              </div>
-              <div class="form-group col-md-4">
-                <label for="inputState">Country</label>
-                <select id="inputState" class="form-control">
-                  <option selected>Choose...</option>
-                  <option>Kenya</option>
-                </select>
-              </div>
-              <div class="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" class="form-control" id="inputZip" />
-              </div>
+            <div class="form-group w-100">
+              <label for="inputPhone">Phone Number</label>
+              <input type="text" class="form-control" required />
+            </div>
+            <div class="form-group w-100">
+              <label for="inputID">ID/Passport No.</label>
+              <input type="text" class="form-control" required />
+            </div>
+            <div class="form-group w-100">
+              <label for="inputID">Date Of Birth</label>
+              <input type="date" class="form-control" required />
+            </div>
+            <div class="form-group w-100">
+              <label for="inputCity">City</label>
+              <input type="text" class="form-control" required />
+            </div>
+            <div class="form-group w-100">
+              <label for="inputState">Country</label>
+              <input type="text" class="form-control" id="inputState" />
             </div>
           </form>
           <h2 class="mt-4 mb-3">Baggage and Pets</h2>
           <form action="" class="mx-3">
             <div class="mb-3">
-              <p>Number of Bags between 23 & 35kgs:</p>
-              <label
-                for="inputbaggage"
-                class="mr-2"
-                style="color: grey; float: left"
-                >2,500 per bag</label
-              >
+              <div>Number of Bags between 23 & 35kgs</div>
+              <div for="inputbaggage" style="color: grey">2,500 per bag</div>
               <input
                 type="number"
-                class="form-control col-md-3"
+                class="form-control w-100"
                 id="inputbaggage"
               />
             </div>
             <div class="mb-3">
-              <p>Number of Pets:</p>
-              <label
-                for="inputPet"
-                class="mr-2"
-                style="color: grey; float: left"
-                >1000 per pet</label
-              >
-              <input
-                type="number"
-                class="form-control col-md-3"
-                id="inputPet"
-              />
+              <div>Number of Pets:</div>
+              <div for="inputbaggage" style="color: grey">1000 per pet</div>
+              <input type="number" class="form-control w-100" id="inputPet" />
+            </div>
+            <div class="mb-3">
+              <button type="button" class="btn btn-success">Done</button>
             </div>
           </form>
-
-          <div class="mb-4">
-            <a href="" style="float: left; color: black"
-              >&#60; Back to Select flight</a
-            >
-            <div
-              class="text-white card col-sm-3"
-              style="float: right; background-color: #e91d24; height: 30px"
-            >
-              <p class="mx-auto card-text">Proceed to payment</p>
-            </div>
-          </div>
         </div>
       </div>
     </div>
